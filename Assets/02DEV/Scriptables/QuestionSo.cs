0ccf8650090e73e5b62960ed5568e3892be265a8
HakkiSahin <34152747+HakkiSahin@users.noreply.github.com>
2025-02-07 22:32:57 +0300

    first commit

---
+++ Assets/02DEV/Scriptables/QuestionSo.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestionSo", menuName = "Scriptable Objects/QuestionSo")]
+public class QuestionSo : ScriptableObject
+{
+    public string questionIndex;
+    public Sprite questionIcon;
+}
